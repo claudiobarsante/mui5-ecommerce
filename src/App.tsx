05c53ddb67b4b44ea4be7e47a4a458edf6f7326d
Claudio Barsante <clbmribas@gmail.com>
2022-09-27 18:30:39 -0300

    feat:<PromotionsSlider/>

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import { Container, ThemeProvider } from '@mui/material';
 import theme from 'styles/theme';
 import Appbar from 'components/Appbar';
-import HeroBanner from 'components/Banner';
+import HeroBanner from 'components/HeroBanner';
+import PromotionsSlider from 'components/PromotionsSlider';
 
 function App() {
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
       <Container maxWidth="xl" sx={{ background: '#fff' }}>
         <Appbar />
         <HeroBanner />
+        <PromotionsSlider />
       </Container>
     </ThemeProvider>
   );
